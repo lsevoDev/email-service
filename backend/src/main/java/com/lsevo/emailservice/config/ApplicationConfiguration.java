4a1dcf7c1baf99d551251a9e4c527272f197d2b7
lsevo <lsevo.dev@gmail.com>
2023-06-04 14:41:51 +0200

    remove redundant object mapper

--- backend/src/main/java/com/lsevo/emailservice/config/ApplicationConfiguration.java
+++ backend/src/main/java/com/lsevo/emailservice/config/ApplicationConfiguration.java
@@ -1,8 +1,5 @@
 package com.lsevo.emailservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -18,10 +15,4 @@ public class ApplicationConfiguration {
         return Clock.systemUTC();
     }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    }
 }
